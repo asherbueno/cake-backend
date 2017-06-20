c5ac6e3fd0dd341f7846f2335eb30c5a4f7a211b
Asher Bueno <ash3rbueno@gmail.com>
2017-06-20 12:47:50 +1000

    added single item page

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -75,3 +75,11 @@ exports.updateStore = async (req, res) => {
   res.redirect(`/stores/${store._id}/edit`);
   // Redriect them the store and tell them it worked
 };
+
+exports.getStore] = async (req, res, next) => {
+  const store = await Store.findOne({ slug: req.params.slug });
+  if(!store) return next();
+  res.render('store', { store, title: store.name });
+  }
+
+};
